05c0cf244ddfb7c318c941e9b6699ab47a3b38c1
yaadevmohit <mohityadav530@gmail.com>
2026-05-25 22:27:23 -0400

    name update

--- components/UserGreeting.tsx
+++ components/UserGreeting.tsx
@@ -2,7 +2,7 @@ export default function UserGreeting() {
   return (
     <div className="my-6">
       <h1 className="text-3xl font-semibold tracking-tight text-gray-900">
-        Good afternoon, Jordan
+        Good afternoon, ye
       </h1>
       <p className="ml-1 mt-1 text-sm tracking-wide text-gray-500">
         You've not practiced anything today brother
